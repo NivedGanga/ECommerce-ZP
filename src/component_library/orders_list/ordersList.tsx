f8c47685e3d22110de9378104fb31b81c767e215
NivedGanga <nivedganga@gmail.com>
2025-07-02 17:30:57 +0530

    fix: removed unused imports

--- src/component_library/orders_list/ordersList.tsx
+++ src/component_library/orders_list/ordersList.tsx
@@ -1,7 +1,6 @@
 import { Grid } from '@mui/material'
 import React from 'react'
 import { useOrdersList } from './useOrdersList'
-import ItemShowCard from '@/shared_features/display_elements/item_show_card/itemShowCard'
 
 const OrdersList = () => {
     const { loading, orders } = useOrdersList()
