2ea506d63be0ac7a916e3a10fe8973057c041e11
NivedGanga <nivedganga@gmail.com>
2025-07-10 22:40:26 +0530

    feat: empty cart and empty wishlist added

--- src/component_library/cart_items/cartItems.tsx
+++ src/component_library/cart_items/cartItems.tsx
@@ -1,28 +1,52 @@
 'use client'
 import { IRootState } from '@/core_components/redux/store'
 import CartItemCard from '@/shared_features/display_elements/cart_item_card/cartItemCard'
-import { Grid } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
+import Image from 'next/image'
 import React from 'react'
 import { useSelector } from 'react-redux'
+import emptyCart from '../../../public/assets/images/empty_cart.png'
+import Column from '@/shared_features/display_elements/column/column'
 
 const CartItems = () => {
   const cartstate = useSelector((state: IRootState) => state.cart)
   console.log(cartstate.cartItems)
   return (
-    <Grid container rowSpacing={3} columnSpacing={2} width={'100%'}>
+    <>
       {
-        cartstate.cartItems.map((cartItem, k) => (
-          <Grid key={k} size={{
-            xs: 12,
-            sm: 6,
-            md: 4
-          }}>
-            <CartItemCard loading={cartstate.updatingItem === cartItem.product.pid} cartItem={cartItem} />
-          </Grid>
-        ))
-      }
+        cartstate.cartItems.length == 0 ?
+          <Grid container width={'100%'} height={'max-content'} sx={{ placeContent: 'center', placeItems: 'center' }}>
+            <Column stackProps={{ alignItems: 'center',gap:2 }}>
+              <Image
+                style={{
+                  placeSelf: 'center'
+                }}
+                src={emptyCart}
+                alt='empty-cart'
+                width={'300'}
+              />
+              <Typography color='textDisabled'>
+                No Items in Cart
+              </Typography>
+            </Column>
+          </Grid> : <Grid
+            rowSpacing={3} columnSpacing={2} width={'100%'}>
+            {
+              cartstate.cartItems.map((cartItem, k) => (
+                <Grid key={k} size={{
+                  xs: 12,
+                  sm: 6,
+                  md: 4
+                }}>
+                  <CartItemCard loading={cartstate.updatingItem === cartItem.product.pid} cartItem={cartItem} />
+                </Grid>
+              ))
+            }
 
-    </Grid>
+          </Grid>
+      }
+    </>
+
   )
 }
 
