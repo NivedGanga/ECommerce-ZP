9028cf144c47ebed95a7de03b062eb99810fcd95
NivedGanga <nivedganga@gmail.com>
2025-07-11 10:12:58 +0530

    fix: order summary is hiding if the cart is empty

--- src/component_library/order_summary/orderSummary.tsx
+++ src/component_library/order_summary/orderSummary.tsx
@@ -11,45 +11,49 @@ const OrderSummary = () => {
     const cartState = useSelector((state: IRootState) => state.cart)
     const router = useRouter()
     return (
-        <Box border={1}
-            overflow={'auto'}
-            width={{
-            xs: '100%'
-        }} padding={5} height={'fit-content'} flex={2}>
-            <CapitalizedText>
-                Order Summary
-            </CapitalizedText>
-            <Stack marginTop={3} gap={1}>
-                {
-                    cartState.cartItems.map((cartItem, k) => (
-                        <Box key={k} display={'flex'} gap={1} justifyContent={'space-between'}>
-                            <Typography sx={{ fontSize: '14px' }}>
-                                {cartItem.product.name}
-                            </Typography>
-                            <Typography sx={{ fontSize: '14px', textWrap: 'nowrap' }}>
-                                {cartItem.product.currentPrice}x {cartItem.quantity}
-                            </Typography>
-                        </Box>
-                    ))
-                }
-            </Stack>
-            <Divider sx={{ margin: '10px 0', borderColor: 'gray' }} />
-            <Box display={'flex'} marginBottom={3} justifyContent={'space-between'}>
+        <>{
+            cartState.cartItems.length > 0 && <Box border={1}
+                overflow={'auto'}
+                width={{
+                    xs: '100%'
+                }} padding={5} height={'fit-content'} flex={2}>
                 <CapitalizedText>
-                    Total
+                    Order Summary
                 </CapitalizedText>
-                <Typography>
+                <Stack marginTop={3} gap={1}>
                     {
-                        cartState.total
+                        cartState.cartItems.map((cartItem, k) => (
+                            <Box key={k} display={'flex'} gap={1} justifyContent={'space-between'}>
+                                <Typography sx={{ fontSize: '14px' }}>
+                                    {cartItem.product.name}
+                                </Typography>
+                                <Typography sx={{ fontSize: '14px', textWrap: 'nowrap' }}>
+                                    {cartItem.product.currentPrice}x {cartItem.quantity}
+                                </Typography>
+                            </Box>
+                        ))
                     }
-                </Typography>
+                </Stack>
+                <Divider sx={{ margin: '10px 0', borderColor: 'gray' }} />
+                <Box display={'flex'} marginBottom={3} justifyContent={'space-between'}>
+                    <CapitalizedText>
+                        Total
+                    </CapitalizedText>
+                    <Typography>
+                        {
+                            cartState.total
+                        }
+                    </Typography>
+                </Box>
+                <StyledButton
+                    onClick={() => router.push('/checkout')}
+                    variant='contained'>
+                    Continue
+                </StyledButton>
             </Box>
-            <StyledButton
-                onClick={() => router.push('/checkout')}
-                variant='contained'>
-                Continue
-            </StyledButton>
-        </Box>
+        }
+        </>
+
     )
 }
 
